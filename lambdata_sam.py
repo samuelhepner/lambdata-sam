495059567365e9a02e7977e2d1bd2565fd48c3c3
SamH3pn3r <samuelhepner03@gmail.com>
2019-09-03 14:47:57 -0700

    'style'

--- lambdata_sam.py
+++ lambdata_sam.py
@@ -1,21 +1,34 @@
 """
     Utility functions for working with dataframes
 """
-
+# The imports
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import numpy as np
 
+# Building a dictionary to then become a dataframe
 sales = {'Account': ['Jones LLC', 'Alpha Co', 'Blue Inc'],
-        'Jan': [150, 200,30],
-        'Feb': [210,300,12],
-        'Mar': [50,50,10]}
-# for testing
+         'Jan': [150, 200, 30],
+         'Feb': [210, 300, 12],
+         'Mar': [50, 50, 10]}\
+
+# A dataframe built from the dictionary above
 df = pd.DataFrame.from_dict(sales)
 
-def findNull(df):
-    null = df.isna().sum()
-    print(null)
 
-def split(df, train_size, test_size):
-    train, validate, test = train_test_split(df, train_size=train_size, test_size=test_size, random_state=42)
-    return train, validate, test
+class cleanDf:
+    # Finds the nulls and displays how many
+    # there are in each column
+    def findNull(df):
+        null = df.isna().sum()
+        print(null)
+
+    # Helps split your data up and you pick how
+    def split(df, train_size, test_size):
+        train, validate, test = train_test_split(
+                                                df, 
+                                                train_size=train_size, 
+                                                test_size=test_size, 
+                                                random_state=42
+                                                )
+        return train, validate, test
