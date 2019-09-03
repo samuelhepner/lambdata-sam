09abecb97ae8fc5a0667343cb5d5f0a42b479d50
SamH3pn3r <samuelhepner03@gmail.com>
2019-09-02 17:05:23 -0700

    'stupid'

---
+++ lambdata_SamH3pn3r/setup.py
@@ -0,0 +1,35 @@
+"""
+
+lambdata - a collection of DS helper functions
+
+"""
+
+import setuptools
+
+REQUIRED = [
+    "numpy",
+    "pandas",
+    "matplotlib",
+    "scikit-learn"
+]
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setuptools.setup(
+    name="lambdata-sam",
+    version="0.0.1",
+    author="SamH3pn3r",
+    description="A collection of DS helper functions",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SamH3pn3r/lambdata-sam",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    install_requires=REQUIRED,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)    
