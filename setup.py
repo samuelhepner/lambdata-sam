b27e4c2508fab2c84c194f32f59e0f33cdd73573
SamH3pn3r <samuelhepner03@gmail.com>
2019-09-02 17:07:18 -0700

    'fixed'

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setuptools.setup(
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     url="https://github.com/SamH3pn3r/lambdata-sam",
-    packages=setuptools.findpackages(),
+    packages=setuptools.find_packages(),
     python_requires=">=3.5",
     install_requires=REQUIRED,
     classifiers=[
